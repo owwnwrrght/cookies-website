4d6fd76c9fbab93eb9d688e46841a8479b388f5a
Owen Wright <owwnwrrght@gmail.com>
2025-12-29 00:01:32 -0700

    Update 2025-12-29 00:01:30

--- src/components/Maintenance.jsx
+++ src/components/Maintenance.jsx
@@ -62,7 +62,7 @@ const Maintenance = ({ onUnlock }) => {
         </form>
       </div>
       
-      <style jsx>{`
+      <style>{`
         @keyframes shake {
           0%, 100% { transform: translateX(0); }
           25% { transform: translateX(-5px); }
